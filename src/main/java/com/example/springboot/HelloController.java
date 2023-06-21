61ebd3956573e49342f96e3784859acdc6937bef
Nick James <verbitan@users.noreply.github.com>
2023-06-21 10:56:03 +0000

    Switch to manual instrumentation

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,6 +3,8 @@ package com.example.springboot.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.amazonaws.xray.interceptors.TracingInterceptor;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.batch.BatchClient;
 
@@ -13,6 +15,9 @@ public class HelloController {
     public String index() {
         BatchClient batchClient = BatchClient.builder()
                                              .region(Region.of("eu-west-1"))
+                                             .overrideConfiguration(ClientOverrideConfiguration.builder()
+                                                 .addExecutionInterceptor(new TracingInterceptor())
+                                                 .build())
                                              .build();
 
         return "Greetings from Spring Boot!";
